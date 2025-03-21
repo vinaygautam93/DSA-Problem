cdbe592b5ca0d5c9468998d07df19cd64b6c5b8d
Vinay Gautam <121504999+vinaygautam93@users.noreply.github.com>
2025-03-21 11:05:12 +0530

    Time: 0 ms (100%), Space: 9.9 MB (97.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        int a = 0, b = 0;
+        int a = 0 ;
+        int b = 0;
         for(int i = 0; i < n; i++) {
             int c = max(a, nums[i] + b);
             b = a; a = c;

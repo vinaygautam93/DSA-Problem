39d25350d5a24f4b9b83bd027416406d95184baa
Vinay Gautam <121504999+vinaygautam93@users.noreply.github.com>
2025-03-30 08:28:20 +0530

    Time: 12 ms (81.66%), Space: 46 MB (28.93%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,38 +1,34 @@
 import java.util.*;
+//import javafx.util.Pair;
+
 class Solution {
     public String frequencySort(String s) {
         
-        int len = s.length();
+        Map<Character, Integer> map = new HashMap<>();
 
-        Map< Character , Integer > map = new HashMap<>();
-
-        for( char it : s.toCharArray() ) {
-            map.put( it , map.getOrDefault( it , 0) + 1);
+        // Count frequency of each character
+        for (char it : s.toCharArray()) {
+            map.put(it, map.getOrDefault(it, 0) + 1);
         }
 
-        //List< Pair< Integer , Character > > list = new ArrayList<>();
-         List<AbstractMap.SimpleEntry<Integer, Character>> list = new ArrayList<>();
+        // Using List of Pairs to store (frequency, character)
+        List<Pair<Integer, Character>> list = new ArrayList<>();
 
-        for( Map.Entry< Character , Integer > it : map.entrySet() ) {
-              
-              char k = it.getKey();
-              int  v = it.getValue();
-              
-              list.add( new AbstractMap.SimpleEntry <>( v , k));
-            
+        for (Map.Entry<Character, Integer> it : map.entrySet()) {
+            list.add(new Pair<>(it.getValue(), it.getKey()));
         }
-        //list.sort();
-        list.sort((a, b) -> b.getKey() - a.getKey());  // dec
-        //list.sort((a, b) -> a.getKey() - b.getKey()); // asc
-        StringBuilder ans = new StringBuilder("");
-        for( AbstractMap.Entry<Integer, Character> it : list  ) {
 
-              int count = it.getKey();
-              char ch = it.getValue();
-              while( count-- > 0 ) {
-                  ans.append(ch);
-              }
+        // Sort in descending order based on frequency
+        list.sort((a, b) -> b.getKey() - a.getKey());
 
+        // Construct the result string
+        StringBuilder ans = new StringBuilder();
+        for (Pair<Integer, Character> it : list) {
+            int count = it.getKey();
+            char ch = it.getValue();
+            while (count-- > 0) {
+                ans.append(ch);
+            }
         }
         return ans.toString();
     }

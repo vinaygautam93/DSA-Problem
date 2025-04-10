31ca54417313eade7e90ddba9d5c02c8b1d98623
Vinay Gautam <121504999+vinaygautam93@users.noreply.github.com>
2025-04-10 17:41:32 +0530

    Time: 0 ms (100%), Space: 8.1 MB (6.67%) - LeetHub

---
+++ 2630-alternating-digit-sum/2630-alternating-digit-sum.cpp
@@ -0,0 +1,34 @@
+// #import< bits/stdc++.h>
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+        
+        stack < int > st;
+        while( n > 0 ) {
+            int rem = n % 10;
+            st.push(rem);
+            n= n / 10;
+        }
+        int ans  =0;
+        bool flag1 = true;
+        bool flag2 = false;
+
+        while( !st.empty() ) {
+            if( flag1 ) {
+                flag1 = false;
+                flag2 = true;
+                
+                ans += st.top();
+                st.pop();
+            }
+            else if( flag2 ) {
+                flag2 = false;
+                flag1 = true;
+                ans-= st.top();
+                st.pop();
+            }
+        }
+        return ans;
+
+    }
+};

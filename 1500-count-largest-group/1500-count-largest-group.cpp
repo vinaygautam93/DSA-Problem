e962c890f252fd363fab8d71bb37be6e8de0abb3
Vinay Gautam <121504999+vinaygautam93@users.noreply.github.com>
2025-04-23 23:53:28 +0530

    Time: 3 ms (33.9%), Space: 8.2 MB (44.07%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countLargestGroup(int n) {
+     int vvt;
+     unordered_map<int, int> digitCountFreq;
+        int maxDigitCount = 0, maxDigitFreq = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int digitSum = 0, key = i;
+            while (key > 0) {
+                digitSum += key % 10;
+                key /= 10;
+            }
+            digitCountFreq[digitSum]++;
+            maxDigitCount = max(maxDigitCount, digitCountFreq[digitSum]);
+        }
+
+        for (auto digit : digitCountFreq) {
+            if (digit.second == maxDigitCount) {
+                maxDigitFreq++;
+            }
+        }
+        return maxDigitFreq;   
+    }
+};

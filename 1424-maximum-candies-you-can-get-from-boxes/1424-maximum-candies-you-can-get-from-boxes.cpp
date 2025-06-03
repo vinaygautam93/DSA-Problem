6764ed90de0e7f1fc3c887e6e890c760dcaeb52b
Vinay Gautam <121504999+vinaygautam93@users.noreply.github.com>
2025-06-03 23:57:43 +0530

    Time: 0 ms (100%), Space: 43.9 MB (97.12%) - LeetHub

---
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int tt=0;
+    int dfs(int i, vector<int>& status, vector<int>& candies,
+            vector<vector<int>>& keys, vector<vector<int>>& containedBoxes) {
+        int ans = candies[i];
+        status[i] = 0; // closed
+        // Collect keys yo open locked boxes
+        for (int k : keys[i]) {
+            status[k] |= 1; // can open
+            if (status[k] == 3)
+                ans += dfs(k, status, candies, keys, containedBoxes);
+        }
+        for (int j : containedBoxes[i]) {
+            status[j] |= 2; // having box j
+            if (status[j] == 3)
+                ans += dfs(j, status, candies, keys, containedBoxes);
+        }
+        return ans;
+    }
+    int maxCandies(vector<int>& status, vector<int>& candies,
+                   vector<vector<int>>& keys,
+                   vector<vector<int>>& containedBoxes,
+                   vector<int>& initialBoxes) {
+        const int n = status.size();
+        int ans = 0;
+        for (int i : initialBoxes) {
+            status[i] |= 2;
+            if (status[i] == 3)
+                ans += dfs(i, status, candies, keys, containedBoxes);
+        }
+        return ans;
+    }
+};

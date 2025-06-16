39ba899ec27dfaa153a109c5f1cf6262e5e6c052
Vinay Gautam <121504999+vinaygautam93@users.noreply.github.com>
2025-06-17 00:16:11 +0530

    Time: 0 ms (100%), Space: 12.3 MB (6.27%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -1,42 +1,43 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
+        
         int n = nums.size();
+        
+        vector<int>preMin;
+        vector<int>postMax;
 
-        // Step 1: Create prefix minimum array
-        vector<int> preMin;
+
         int mn = nums[0];
         preMin.push_back(mn);
 
-        for (int i = 1; i < n; i++) {
-            mn = min(nums[i], mn);
+        for( int i = 1; i < n; i++ ) {
+            mn = min( nums[i] , mn);
             preMin.push_back(mn);
         }
-
-        // Step 2: Create postfix maximum array
-        vector<int> postMax;
-        int mx = nums[n - 1];
+        int mx = nums[n-1];
         postMax.push_back(mx);
-
-        for (int i = n - 2; i >= 0; i--) {
-            mx = max(nums[i], mx);
+        for( int i = n-2; i >=0; i--) {
+            mx = max( nums[i] , mx);
             postMax.push_back(mx);
         }
-
-        reverse(postMax.begin(), postMax.end());
-
-        // Step 3: Calculate maximum difference
+        reverse( postMax.begin(), postMax.end());
         int diff = 0;
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++ ) {
+               
             int tempDiff = postMax[i] - preMin[i];
-            diff = max(diff, tempDiff);
-        }
+            diff = max( diff , tempDiff);
 
-        // Step 4: Return result
+        }
+        // if( diff == 0) {
+        //     return -1;
+        // }
+        //  return diff;
         return (diff == 0) ? -1 : diff;
+
+        
+
+
+
     }
 };

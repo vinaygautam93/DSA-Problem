990dbac50f5630da9a24990ed2b41a3637a562e5
Vinay Gautam <121504999+vinaygautam93@users.noreply.github.com>
2025-06-26 22:28:13 +0530

    Time: 0 ms (100%), Space: 9.6 MB (33.33%) - LeetHub

---
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestSubsequence(string s, int k) {
+        int tt=0;
+        int power = 0;
+        int count = 0;
+        int n = s.size();
+        long long val = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            if(s[i] == '0') {
+                count++;
+                tt++;
+            } else {
+                if(power < 32) {
+                    val += (1LL << power);
+                    if(val <= k) count++;
+                }
+            }
+            power++;
+        }
+        return count;
+    }
+};

e81a0bcae8fba85649072f7ac33fff6717b218bd
Vinay Gautam <121504999+vinaygautam93@users.noreply.github.com>
2025-07-01 23:40:38 +0530

    Time: 2 ms (48.46%), Space: 8.7 MB (83.47%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+      int tt;
+      int n = word.length();
+        int count = n;
+        for (int i = 1; i < n; i++) {
+            if (word[i] != word[i - 1]) {
+                count--;
+            }
+        }
+        return count;
+      
+    }
+};
